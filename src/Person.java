8650f8f55064bcd352aa04a39fdf246b8e1f8241
ANA KHASANAH <ana.khasanah@capita.com>
2023-06-05 19:47:43 +0100

    simple OOP with method constructor

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    // declare all attributes
+
+    String fName;
+    String lName;
+    int age;
+    String house;
+
+    // method constructor
+    public Person(String fName, String lName, int age, String house) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.house = house;
+    }
+}
+
